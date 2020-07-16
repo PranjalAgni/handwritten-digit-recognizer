8f23c4dcf0c55e1d965a85f18ab58b05b1083fec
Pranjal Agnihotri <pranjal.agnihotri6@gmail.com>
2020-07-17 00:14:12 +0530

    :art: styles: used grid layout to center

--- src/components/shared/CenterCol.js
+++ src/components/shared/CenterCol.js
@@ -2,9 +2,8 @@ import { Col } from 'antd';
 import styled from 'styled-components';
 
 const CenterCol = styled(Col)`
-  display: flex;
-  align-items: center;
-  justify-content: center;
+  display: grid;
+  place-items: center;
 `;
 
 export default CenterCol;
