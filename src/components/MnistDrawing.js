8c78e959eebef2c4aa3516887916a4dcb7ca9e3f
Pranjal Agnihotri <pranjal.agnihotri6@gmail.com>
2020-07-17 00:13:26 +0530

    :hammer: refactor: code cleaning

--- src/components/MnistDrawing.js
+++ src/components/MnistDrawing.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Spin } from 'antd';
+import { Spin, Row } from 'antd';
 import CenterCol from './shared/CenterCol';
 import DrawingContainer from '../containers/Drawing';
 import OutputContainer from '../containers/Output';
@@ -9,11 +9,7 @@ export default function MnistDrawing({ spinning }) {
     <Spin spinning={spinning}>
       <CenterCol xs={24}>
         <DrawingContainer />
-      </CenterCol>
-      <CenterCol xs={24}>
         <OutputContainer />
-      </CenterCol>
-      <CenterCol xs={24}>
         <ClearContainer />
       </CenterCol>
     </Spin>
