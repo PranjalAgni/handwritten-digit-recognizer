b4d63da09101787e7e71444c368a67823b0df0bb
Pranjal Agnihotri <pranjal.agnihotri6@gmail.com>
2020-07-17 00:15:48 +0530

    :zap: fix: dispatching action to reset prediction

--- src/containers/Clear.js
+++ src/containers/Clear.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { resetDrawing } from '../actions/drawing';
+import { resetPrediction } from '../actions/mnist';
 import Clear from '../components/Clear';
 export default function ClearContainer() {
   const dispatch = useDispatch();
   const drawingDispatch = {
-    resetDrawing: () => dispatch(resetDrawing())
+    resetDrawing: () => {
+      dispatch(resetDrawing());
+      dispatch(resetPrediction());
+    },
   };
   return <Clear {...drawingDispatch} />;
 }
